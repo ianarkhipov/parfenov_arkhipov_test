6782af876b02f4db0bc621c373fd4a09bd6158bf
Ian.arkhipov <85026167+interiorengine@users.noreply.github.com>
2024-01-23 00:35:12 +0300

    Rev

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/app/app.component.html",
+    "./src/app/app.component.css"
+  ],
+  theme: {
+    colors: {
+    'background-basic': '#F9F9F9',
+    'black-brand': '#101D2D',
+    'blue-brand': '#4C54F4',
+    'grey-dark': '#6F727A',
+    'table-stroke': '#E0E2E7',
+    'true-white': '#FFFFFF',
+    },
+    extend: {
+      fontFamily: {
+        main: ['Inter'],
+      },
+    },
+  },
+  plugins: [
+
+  ],
+}
+
