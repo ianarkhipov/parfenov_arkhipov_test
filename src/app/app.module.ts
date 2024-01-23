f0c624e4e334da1c7d3004d0ffff36e906fa7a8c
Ian.arkhipov <85026167+interiorengine@users.noreply.github.com>
2024-01-23 15:10:13 +0300

    Component env is set for admin dash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {CommonModule} from "@angular/common";
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    NotFoundComponent,
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
